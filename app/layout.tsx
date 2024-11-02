4af2067caf2803afe54b0a3c83289817d202010a
tsuchiya-yu <nomoreheronomorehero@gmail.com>
2024-11-02 17:35:44 +0900

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,34 @@
+"use client";
+
+import * as React from 'react';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#1976d2',
+    },
+    secondary: {
+      main: '#dc004e',
+    },
+  },
+});
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="jp">
+      <head />
+      <body>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
